a94904fa0d42877f3dd8399a31b524d9a549943c
Nerd1561 <684244137@mail.pbru.ac.th>
2025-12-25 11:32:00 +0700

    Update lab3-7.cpp

--- Lab3/lab3-7.cpp
+++ Lab3/lab3-7.cpp
@@ -2,17 +2,17 @@
 #include <thread>
 #include <chrono>
 void worker(int id) {
-for (int i = 0; i < 5; i++) {
-std::cout << "Worker " << id << " running...\n";
-std::this_thread::sleep_for(std::chrono::milliseconds(500));
-}
+  for (int i = 0; i < 5; i++) {
+    std::cout << "Worker " << id << " running...\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
 }
 int main() {
 
-std::thread t1(worker, 1);
-std::thread t2(worker, 2);
-t1.join();
-t2.join();
-std::cout << "All threads finished.\n";
-return 0;
+  std::thread t1(worker, 1);
+  std::thread t2(worker, 2);
+  t1.join();
+  t2.join();
+  std::cout << "All threads finished.\n";
+  return 0;
 }
